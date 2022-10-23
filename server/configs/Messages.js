35d905aee4c553df524d8b1a4b72f6e0ab27dd1b
Your Name <you@example.com>
2022-10-23 16:20:30 +0400

    vdsv

--- server/configs/Messages.js
+++ server/configs/Messages.js
@@ -1,3 +1,4 @@
+
 export const VALIDATION_ERROR = 'Ошибка валидаии'
 export const DOES_NOT_VALID_EMAIL = 'Это не валидный email'
 export const REQUIRED_NAME = 'Имя обязително'
@@ -7,6 +8,8 @@ export const PASSWORD_INCORRECT = 'Парол не правилый'
 export const AUTORIZED_ERROR = 'Нет аутентификации'
 export const ADMIN_ERROR = 'Ты не являешся админом'
 export const INCORRECT_LINK = 'Это не правилнйй адресс'
+export const SUCCESS_SIGNUP = 'Вы успешно зарегистрировани, проверте ваша электронная почта'
+export const SUCCESS_SEND_RESET_PASS = 'Для изменение пароля, зайдите вашу электронную почту'
 
 export function getMessage (type, value = ''){
     switch (type) {
@@ -14,10 +17,7 @@ export function getMessage (type, value = ''){
             return `Ваш ${value} email уже исползуется, попробуйте другой email`
         case 'error-nickname':
             return `${value}, это имя уже исползуется, попробуйте другое имя`
-        case 'success-signUp':
-            return `Вы успешно зарегистрировани, проверте ваша электронная почта`
         default:
             break;
     }
 }
-
--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -5,11 +5,19 @@ import bcrypt from 'bcrypt'
 import { UserDataToken_dto } from '../dto/UserDataToken_dto.js';
 import { validationResult } from "express-validator";
 import { User_dto } from './../dto/User_dto.js';
-import {getMessage, VALIDATION_ERROR} from './../configs/Messages.js';
+import {
+    EMAIL_INCORRECT,
+    getMessage, INCORRECT_LINK,
+    SUCCESS_SEND_RESET_PASS,
+    SUCCESS_SIGNUP,
+    VALIDATION_ERROR
+} from './../configs/Messages.js';
 import logger from '../logger/Logger.js';
 import * as uuid from 'uuid';
 import MailServices from '../services/MailServices.js';
 import RoleModal from '../database/models/RoleModal.js';
+import ApiError from "../error/ApiError.js";
+import jwt from "jsonwebtoken";
 import Logger from "../logger/Logger.js";
 
 class AuthController {
@@ -26,14 +34,10 @@ class AuthController {
             const userRole = await RoleModal.findOne({value: 'user'})
             await UserModel.create({email, password: hashPassword, nickname, activationLink, role: [userRole.value]})
             await MailServices.sendActivationMail(email, `${process.env.API_WEB_URL}/api/auth/activate/${activationLink}`)
-            //const user = new User_dto(User)
-            // const UserDataToken = new UserDataToken_dto(User)
-            // const tokens = TokenServices.generateTokens({...UserDataToken})
-            // await TokenServices.saveToken(UserDataToken.id, tokens.refreshToken)
-            // res.cookie('refreshToken', tokens.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
             Logger.info(`User ${nickname} sign up`, { message: 'world' });
-            res.json({message: getMessage('success-signUp')})
+            res.json({message: SUCCESS_SIGNUP})
         }catch(e){
+            Logger.error(e)
             next(e)
         }
     }
@@ -46,12 +50,12 @@ class AuthController {
             const UserDataToken = new UserDataToken_dto(user)
             const tokens = TokenServices.generateTokens({...UserDataToken})
             await TokenServices.saveToken(UserDataToken.id, tokens.refreshToken)
-            res.cookie('refreshToken', tokens.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
             res.json({
                 ...tokens,
                 User
             })
         } catch (e) {
+            Logger.error(e)
             next(e)
         }
     }
@@ -62,20 +66,23 @@ class AuthController {
             const token = await TokenServices.removeToken(refreshToken)
             res.clearCookie('refreshToken')
             return res.json(token)
-        } catch (error) {
-            next(error)
+        } catch (e) {
+            Logger.error(e)
+            next(e)
         }
     }
 
     async refresh(req, res, next){
         try {
             const {refreshToken} = req.cookies
+            console.log(refreshToken, ' refreshToken')
             const userData = await UserServices.refresh(refreshToken)
 
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
             return res.json(userData)
-        } catch (error) {
-            next(error)
+        } catch (e) {
+            Logger.error(e)
+            next(e)
         }
     }
 
@@ -85,12 +92,54 @@ class AuthController {
             await UserServices.activate(activationLink)
             return res.redirect(process.env.CLIENT_URL)
         }catch (e) {
+            Logger.error(e)
+            next(e)
+        }
+    }
+
+    async forgetPass(req, res, next){
+        try{
+            const {email} = req.body
+            const user = await UserModel.findOne({email})
+            if(!user){
+                throw ApiError.badRequest(EMAIL_INCORRECT)
+            }
+            const passToken = jwt.sign({email}, process.env.FORGET_PASSWORD_TOKEN, {expiresIn: '5m'})
+
+            await MailServices.SendResetPassword(email, `${process.env.API_WEB_URL}/api/auth/resetPass/${passToken}`)
+            user.resetPassLink = passToken
+            res.json({message: SUCCESS_SEND_RESET_PASS})
+        }catch (e) {
+            Logger.error(e)
+            next(e)
+        }
+    }
+
+    async resetPass(req, res, next){
+        try{
+            const token = req.params.token
+            const existToken = TokenServices.validateToken(token, process.env.FORGET_PASSWORD_TOKEN)
+            if(!existToken){
+                throw ApiError.badRequest(INCORRECT_LINK)
+            }
+            const {email} = existToken
+            const user = await UserModel.findOne({email})
+            if(!user){
+                throw ApiError.badRequest(INCORRECT_LINK)
+            }
+            if(user.resetPassLink !== token){
+                throw ApiError.badRequest(INCORRECT_LINK)
+            }
+            res.redirect(process.env.CLIENT_URL)
+        }catch (e) {
+            Logger.error(e)
             next(e)
         }
     }
 
     async getUsers(req, res, next){
         try{
+            console.log(req.headers.authorization)
             const users = await UserModel.find()
             return res.json(users)
         }catch (e) {
