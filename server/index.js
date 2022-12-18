48e1c1d4dfc76ac3169334c47e771ca569b11cda
Your Name <you@example.com>
2022-12-18 18:52:31 +0400

    test timestamp in mongoose

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import corsMiddleware from './middleware/CorsMiddleware.js'
 import session from "express-session";
+import logger from "./logger/Logger.js";
+import TestModel from "./database/models/TestModel.js";
 
 
 const PORT = process.env.PORT || 8000
@@ -24,6 +26,34 @@ app.use(cookieParser())
 app.use('/api', router)
 app.use(ErrorHandlingMiddleware)
 
+app.post('/createTest', async (req, res) => {
+    try {
+        const {name} = req.body
+        await TestModel.create({name})
+        res.send('success')
+    }catch (e) {
+        logger.error(e)
+    }
+})
+
+app.get('/test', async (req, res) => {
+    try {
+        const test = await TestModel.find({"updatedAt":{$lt: new Date(Date.now() - 1 * 60 * 1000)}});
+        res.json(test)
+    }catch (e) {
+        logger.error(e)
+    }
+})
+
+app.put('/testChange/:name', async (req, res) => {
+    try {
+        const {name} = req.params
+        const test = await TestModel.findOneAndUpdate({name}, {name: 'change'}, {new: true});
+        res.json(test)
+    }catch (e) {
+        logger.error(e)
+    }
+})
 
 
 
